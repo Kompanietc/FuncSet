f510229b9b7f73dc93cc4e14a945c630a413fe6c
Vladimir Kompanietc <vova.opov@mail.ru>
2015-10-29 18:24:02 +0400

    Count symbols prog added

---
+++ Count_symbols/src/CountSymbols.java
@@ -0,0 +1,25 @@
+﻿import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ *  возвращает количество заданных символов в строке;
+ */
+public class CountSymbols {
+    
+    /**
+     *  функция, отвечающая за подсчет символов;
+     */
+    public  static int count(String str, char symbol) {
+        int count = 0;
+        if (str != null) {
+            for (int i = 0; i < str.length(); i++) {
+                if (str.charAt(i) == symbol) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+}
---
+++ Count_symbols/test/CountSymbolsTest.java
@@ -0,0 +1,19 @@
+﻿import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by admin on 29.10.15.
+ */
+public class CountSymbolsTest {
+
+    @Test
+    public void testCountSymbols() throws Exception {
+        String str = "aaaaaa";
+        char symb = 'a';
+        int _count = 6;
+
+        assertEquals(_count,CountSymbols.count(str,symb));
+
+    }
+}
