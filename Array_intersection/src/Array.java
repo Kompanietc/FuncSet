4f5c7e58a834bc19b1f70127b5de0539e6466019
Vladimir Kompanietc <vova.opov@mail.ru>
2015-10-29 18:27:45 +0400

    Intersection prog added

---
+++ Array_intersection/src/Array.java
@@ -0,0 +1,88 @@
+﻿/**
+ * Пересечение двух массивов
+ */
+public class Array {
+    int n;
+    double [] A;
+    double eps = 0.000001;
+
+    public Array(int _n){
+
+
+        n = _n;
+        A = new double[n];
+    }
+
+    public Array(double[]a){
+        n = a.length;
+
+        A = a;
+    }
+
+    /**
+     * Сравнение двух массивов для тестовой функции
+     */
+    public boolean compare(Array m1){
+
+        if (Math.abs(m1.n - this.n ) > eps)
+            return false;
+        else{
+            for (int i = 0; i < n; i++) {
+
+                if (this.A[i] != m1.A[i])
+                    return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Вывод массива
+     */
+    public void print(){
+        for (int i = 0; i< n; i++){
+
+            System.out.print(A[i]);
+            System.out.print("\t");
+
+
+
+        }
+        System.out.println();
+        System.out.println();
+    }
+
+    /**
+     * функция для определения пересечения двух массивов
+     */
+    public void intersection(Array a, Array b){
+        int k = 0;
+        int count = 0;
+
+        for (int i = 0; i < a.A.length; i++){
+            boolean fl = false;
+
+            for (int p = 0; p < i; p++) {
+                if (Math.abs(a.A[i] - a.A[p]) < eps)
+                    fl = true;
+            }
+            if(!fl) {
+                for (int j = 0; j < b.A.length; j++)
+                    if (Math.abs(a.A[i] - b.A[j] ) < eps) {
+                        A[k] = a.A[i];
+                        k++;
+                        count++;
+                        break;
+                    }
+            }
+        }
+        n = count;
+    }
+
+    /**
+     * Возврат матрицы из экземпляра класса Matrix
+     */
+    public double[] getMassive (){
+        return A;
+    }
+}
---
+++ Array_intersection/test/ArrayTest.java
@@ -0,0 +1,26 @@
+﻿import static org.junit.Assert.assertArrayEquals;
+
+/**
+ * Created by Компаниец on 17.10.2015.
+ */
+public class ArrayTest {
+
+    @org.junit.Test
+    public void testIntersection() throws Exception {
+        double eps = 0.000001;
+        double a [] = {1,2,3,4,0,20.2233};//massive n
+        double b [] = {5,1,2,0,20.2234};//massive m
+        double c [] = {1,2,0};
+
+        int n = 3;
+
+        Array mas = new Array(a);
+        Array mas1 = new Array(b);
+
+        Array result = new Array(n);
+        result.intersection(mas, mas1);
+
+        assertArrayEquals(c,result.getMassive(),eps);
+
+    }
+}
